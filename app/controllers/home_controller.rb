2c795e31fe960b131129471024958b9018db086d
Patrick Mulder <mulder@mtpartners.de>
2012-07-08 22:09:18 +0200

    add guest user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     @network_update = NetworkUpdate.new
     @activities = exhibit(current_user.activity_stream) if current_user
+    @activities = exhibit("You need to signup a user first") if User.guest_user.nil?
     @activities ||= exhibit(User.guest_user.activity_stream)
   end
 
